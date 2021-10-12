122a1760bb54c17b506473c053a6da4f4225c0d5
Nika <64929516+nikule4ka@users.noreply.github.com>
2021-10-12 23:45:52 +0300

    working progress

--- routes/api/cardsRoute.js
+++ routes/api/cardsRoute.js
@@ -2,6 +2,14 @@ const express = require('express')
 const router = express.Router()
 const cardsController = require('../../controllers/сardsController')
 
-router.get('/cards', cardsController.get)
+router
+  .get('/cards', cardsController.get)
+  .post('/cards', cardsController.createCard)
+
+  
+  router
+  .get('/cards/:cardId', cardsController.getById)
+  .delete('/cards/:cardId', cardsController.removeCard)
+  //   .put('/cards/:cardId', cardsController.updateCard)
 
 module.exports = router
