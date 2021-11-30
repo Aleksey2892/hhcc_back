4c0cadb8a8d8108436af3c024b25e5101f83501b
Nika <64929516+nikule4ka@users.noreply.github.com>
2021-11-30 21:45:49 +0200

    Revert "Nika branch"

--- src/api/series/series.router.js
+++ src/api/series/series.router.js
@@ -5,7 +5,7 @@ const guard = require('../../helpers/guard')
 const upload = require('../../helpers/upload')
 
 router
-  .get('/series', SeriesController.get)
+  .get('/series', guard, SeriesController.get)
   .post('/series', upload.single('file'), guard, SeriesController.create)
 
 router
@@ -15,7 +15,7 @@ router
     guard,
     SeriesController.update,
   )
-  .get('/series/:seriesId', SeriesController.getById)
+  .get('/series/:seriesId', guard, SeriesController.getById)
   .delete('/series/:seriesId', guard, SeriesController.remove)
 
 module.exports = router
